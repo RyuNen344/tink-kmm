c040f942f32e7fd8cd6266dd724cd2dd9ebf96c7
RyuNen344 <s1100633@outlook.com>
2023-04-18 19:54:27 +0900

    feat(configs): impl HybridConfig ailas

---
+++ tink/src/iosMain/kotlin/io/github/ryunen344/tink/hybrid/HybridConfig.kt
@@ -0,0 +1,22 @@
+package io.github.ryunen344.tink.hybrid
+
+import io.github.ryunen344.tink.exception.GeneralSecurityException
+import io.github.ryunen344.tink.util.asThrowable
+import kotlinx.cinterop.ObjCObjectVar
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
+import platform.Foundation.NSError
+
+actual class HybridConfig {
+    @Throws(GeneralSecurityException::class)
+    actual fun register() {
+        memScoped {
+            val error = alloc<ObjCObjectVar<NSError?>>()
+            val hybridConfig = TINKHybridConfig(error.ptr)
+            TINKConfig.registerConfig(config = hybridConfig, error = error.ptr)
+            error.value?.let { throw GeneralSecurityException(cause = it.asThrowable()) }
+        }
+    }
+}
