ace4e3b5e846ecb4b0792ae4033d6c3c1323916d
RyuNen344 <s1100633@outlook.com>
2023-04-21 00:18:52 +0900

    refactor(configs): refactor DeterministicAeadConfig

--- tink/src/commonMain/kotlin/io/github/ryunen344/tink/daead/DeterministicAeadConfig.kt
+++ tink/src/commonMain/kotlin/io/github/ryunen344/tink/daead/DeterministicAeadConfig.kt
@@ -2,7 +2,9 @@ package io.github.ryunen344.tink.daead
 
 import io.github.ryunen344.tink.exception.GeneralSecurityException
 
-expect class DeterministicAeadConfig constructor() {
-    @Throws(GeneralSecurityException::class)
-    fun register()
+class DeterministicAeadConfig {
+    companion object
 }
+
+@Throws(GeneralSecurityException::class)
+expect fun DeterministicAeadConfig.Companion.register()
